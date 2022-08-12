ebf1b8bbebd22378a291c30f30f99aa263ca72fc
Nur <furiousnur@gmail.com>
2022-08-12 17:43:52 +0600

    react routing|navigation and redirect

--- src/App.js
+++ src/App.js
@@ -5,20 +5,20 @@ import About from "./Pages/About";
 import Contact from "./Pages/Contact";
 import Blogs from "./Pages/Blogs";
 import Error from "./Pages/Error";
+import Navbar from "./Components/Navbar";
 
 function App() {
-  return (
-    <div className="App">
-        <BrowserRouter>
-            <Routes>
-                <Route path="/home" element={<Home />}/> 
-                <Route path="/about" element={<About />}/> 
-                <Route path="/contact" element={<Contact />}/> 
-                <Route path="/blogs" element={<Blogs />}/> 
-                <Route path="*" element={<Error />}/> 
-            </Routes>
-        </BrowserRouter>
-    </div>
+  return ( 
+    <BrowserRouter> 
+        <Navbar/>
+        <Routes>
+            <Route path="/" element={<Home />}/> 
+            <Route path="/about" element={<About />}/> 
+            <Route path="/contact" element={<Contact />}/> 
+            <Route path="/blogs" element={<Blogs />}/> 
+            <Route path="*" element={<Error />}/> 
+        </Routes>
+    </BrowserRouter> 
   );
 }
 
