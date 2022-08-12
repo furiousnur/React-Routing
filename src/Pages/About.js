f26038c70ea379c6a50570fc173e9ed978c45ee3
Nur <furiousnur@gmail.com>
2022-08-12 17:04:19 +0600

    react router install and used

---
+++ src/Pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>About Page</h1>
+            <p style={{textAlign:"justify"}}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet dolore doloribus eligendi illo modi non, odio quae ut! Autem ea esse ipsam omnis saepe? Cumque dolor eius facilis quidem quisquam totam veniam voluptates voluptatum? Blanditiis ex fuga fugit impedit inventore iure laboriosam modi necessitatibus, nesciunt officiis, optio praesentium quaerat quos ratione repudiandae sequi sunt temporibus unde ut veniam vero voluptatem voluptatum. Animi aspernatur blanditiis consequuntur fugit ipsam iste laborum officiis quia quod sapiente! Amet animi, asperiores cumque debitis deleniti dolore ducimus earum eius explicabo facere fugiat harum, iste maxime non odio placeat qui repellendus rerum, saepe tempore? Qui, repudiandae, veritatis?</p>
+            <p style={{textAlign:"justify"}}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet dolore doloribus eligendi illo modi non, odio quae ut! Autem ea esse ipsam omnis saepe? Cumque dolor eius facilis quidem quisquam totam veniam voluptates voluptatum? Blanditiis ex fuga fugit impedit inventore iure laboriosam modi necessitatibus, nesciunt officiis, optio praesentium quaerat quos ratione repudiandae sequi sunt temporibus unde ut veniam vero voluptatem voluptatum. Animi aspernatur blanditiis consequuntur fugit ipsam iste laborum officiis quia quod sapiente! Amet animi, asperiores cumque debitis deleniti dolore ducimus earum eius explicabo facere fugiat harum, iste maxime non odio placeat qui repellendus rerum, saepe tempore? Qui, repudiandae, veritatis?</p>
+        </div>
+    );
+};
+
+export default About;
